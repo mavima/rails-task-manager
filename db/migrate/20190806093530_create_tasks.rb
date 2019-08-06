8a82447085fa132ef336c940016c7c4f0f078779
Maria Manninen <maria.vihla@gmail.com>
2019-08-06 11:37:31 +0200

    corrected migration

--- db/migrate/20190806093530_create_tasks.rb
+++ db/migrate/20190806093530_create_tasks.rb
@@ -2,11 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :string
       t.string :details
-      t.string :string
-      t.string :completed
-      t.string :boolean
+      t.boolean :completed
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_092353) do
+ActiveRecord::Schema.define(version: 2019_08_06_093530) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
     t.string "details"
-    t.string "completed"
-    t.string "boolean"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
